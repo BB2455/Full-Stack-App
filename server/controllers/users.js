07dd8c06c3bae4ac9519538124a8f82cceb1f691
Blair B <blairbcodes@gmail.com>
2021-12-21 17:27:06 -0330

    added status codes & fixed small issues

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -5,8 +5,11 @@ export const getUsers = async (req, res) => {
   const { page } = req.params;
   try {
     const LIMIT = 12;
-    const startIndex = (Number(page) - 1) * LIMIT;
     const total = await User.countDocuments();
+    const numberOfPages = Math.ceil(total / LIMIT);
+    if (page > numberOfPages || page < 1)
+      return res.status(400).json({ message: "Not a valid page request" });
+    const startIndex = (Number(page) - 1) * LIMIT;
     const users = await User.find()
       .sort({ _id: -1 })
       .limit(LIMIT)
@@ -15,17 +18,18 @@ export const getUsers = async (req, res) => {
     res.status(200).json({
       data: users,
       currentPage: Number(page),
-      numberOfPages: Math.ceil(total / LIMIT),
+      numberOfPages: numberOfPages,
       results: total,
     });
   } catch (error) {
     res.status(404).json({ message: error.message });
-    console.log("PROBLEM");
   }
 };
 
 export const getUser = async (req, res) => {
   const { id: _id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(400).json({ message: "Not a valid id" });
   try {
     const user = await User.findById(_id);
     if (!user) return res.status(404).json({ message: "No user with that id" });
@@ -38,68 +42,73 @@ export const getUser = async (req, res) => {
 export const getUsersBySearch = async (req, res) => {
   const { search, filterBy, startDate, endDate, orderType, order, page } =
     req.query;
-  const LIMIT = 12;
-  const getPage = page || 1;
-  const startIndex = (Number(getPage) - 1) * LIMIT;
-  const searchRegEx = search ? new RegExp(search, "i") : /[\d\D]+/i;
-  const searchName =
-    filterBy === "firstName"
-      ? { first_name: searchRegEx }
-      : filterBy === "lastName"
-      ? { last_name: searchRegEx }
-      : { $or: [{ first_name: searchRegEx }, { last_name: searchRegEx }] };
-  const searchStartDate = startDate && {
-    createdAt: { $gte: new Date(startDate) },
-  };
-  const searchEndDate = endDate && { createdAt: { $lte: new Date(endDate) } };
-  let getOrder;
-  switch (orderType) {
-    case "relevancy":
-      getOrder =
-        filterBy === "lastName"
-          ? { last_name: order === "ascending" ? 1 : -1 }
-          : { first_name: order === "ascending" ? 1 : -1 };
-      break;
-    case "first_name":
-      getOrder = { first_name: order === "ascending" ? 1 : -1 };
-      break;
-    case "last_name":
-      getOrder = { last_name: order === "ascending" ? 1 : -1 };
-      break;
-    case "created_at":
-      getOrder = { createdAt: order === "ascending" ? 1 : -1 };
-      break;
-    case "updated_at":
-      getOrder = { updatedAt: order === "ascending" ? 1 : -1 };
-      break;
-    default:
-      getOrder = { first_name: order === "ascending" ? 1 : -1 };
-      break;
-  }
-
-  const users = await User.find(
-    searchStartDate && searchEndDate
-      ? { $and: [searchName, searchStartDate, searchEndDate] }
-      : searchStartDate
-      ? { $and: [searchName, searchStartDate] }
-      : searchEndDate
-      ? { $and: [searchName, searchEndDate] }
-      : searchName
-  ).sort(getOrder);
-  const total = users.length;
-
-  if (!users) return res.status(404).json({ message: "No Users Found" });
-
-  res.status(200).json({
-    data: users.slice(startIndex, startIndex + LIMIT),
-    currentPage: Number(getPage),
-    numberOfPages: Math.ceil(total / LIMIT),
-    results: total,
-  });
   try {
+    const LIMIT = 12;
+    const getPage = page || 1;
+    const startIndex = (Number(getPage) - 1) * LIMIT;
+    try {
+      const searchRegEx = search ? new RegExp(search, "i") : /[\d\D]+/i;
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ message: "Invalid Character in Search Query" });
+    }
+    const searchName =
+      filterBy === "firstName"
+        ? { first_name: searchRegEx }
+        : filterBy === "lastName"
+        ? { last_name: searchRegEx }
+        : { $or: [{ first_name: searchRegEx }, { last_name: searchRegEx }] };
+    const searchStartDate = startDate && {
+      createdAt: { $gte: new Date(startDate) },
+    };
+    const searchEndDate = endDate && { createdAt: { $lte: new Date(endDate) } };
+    let getOrder;
+    switch (orderType) {
+      case "relevancy":
+        getOrder =
+          filterBy === "lastName"
+            ? { last_name: order === "ascending" ? 1 : -1 }
+            : { first_name: order === "ascending" ? 1 : -1 };
+        break;
+      case "first_name":
+        getOrder = { first_name: order === "ascending" ? 1 : -1 };
+        break;
+      case "last_name":
+        getOrder = { last_name: order === "ascending" ? 1 : -1 };
+        break;
+      case "created_at":
+        getOrder = { createdAt: order === "ascending" ? 1 : -1 };
+        break;
+      case "updated_at":
+        getOrder = { updatedAt: order === "ascending" ? 1 : -1 };
+        break;
+      default:
+        getOrder = { first_name: order === "ascending" ? 1 : -1 };
+        break;
+    }
+
+    const users = await User.find(
+      searchStartDate && searchEndDate
+        ? { $and: [searchName, searchStartDate, searchEndDate] }
+        : searchStartDate
+        ? { $and: [searchName, searchStartDate] }
+        : searchEndDate
+        ? { $and: [searchName, searchEndDate] }
+        : searchName
+    ).sort(getOrder);
+    const total = users.length;
+
+    if (!users) return res.status(404).json({ message: "No Users Found" });
+
+    res.status(200).json({
+      data: users.slice(startIndex, startIndex + LIMIT),
+      currentPage: Number(getPage),
+      numberOfPages: Math.ceil(total / LIMIT),
+      results: total,
+    });
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
-    console.log(error.message);
   }
 };
 
@@ -132,9 +141,10 @@ export const deleteUser = async (req, res) => {
   const { id: _id } = req.params;
   try {
     if (!mongoose.Types.ObjectId.isValid(_id))
-      return res.status(404).send("No user with that id");
+      return res.status(400).send("Not a valid id");
 
-    await User.findByIdAndDelete(_id);
+    const user = await User.findByIdAndDelete(_id);
+    if (!user) return res.status(404).json({ message: "No user with that id" });
     res.status(200).json({ message: "User Deleted Successfully" });
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
