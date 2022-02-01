1fecb66c6b946cda77a25c7043906274874e0cac
Blair B <blairbcodes@gmail.com>
2022-02-01 19:22:21 -0330

    added trim to validation schemas

--- server/schemas/validationSchema.js
+++ server/schemas/validationSchema.js
@@ -2,9 +2,9 @@ import Joi from 'joi'
 
 export const RegisterSchema = Joi.object({
   confirmPassword: Joi.ref('password'),
-  email: Joi.string().email().lowercase().required(),
+  email: Joi.string().email().lowercase().trim().required(),
   password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/u).required(),
-  username: Joi.string().alphanum().min(3).max(30).lowercase().required(),
+  username: Joi.string().alphanum().min(3).max(30).lowercase().trim().required(),
 })
 
 export const DeleteSchema = Joi.object({
@@ -13,7 +13,7 @@ export const DeleteSchema = Joi.object({
 
 export const LoginSchema = Joi.object({
   password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/u).required(),
-  username: Joi.string().alphanum().min(3).max(30).lowercase().required(),
+  username: Joi.string().alphanum().min(3).max(30).lowercase().trim().required(),
 })
 
 export const VerifyEmailSchema = Joi.object({
@@ -21,7 +21,7 @@ export const VerifyEmailSchema = Joi.object({
 })
 
 export const ForgotPasswordSchema = Joi.object({
-  email: Joi.string().email().lowercase().required()
+  email: Joi.string().email().lowercase().trim().required()
 })
 
 export const ChangePasswordSchema = Joi.object({
