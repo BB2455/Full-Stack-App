74cf1681b68bdee938c519dc500034642819d4fc
Blair B <blairbcodes@gmail.com>
2022-08-04 17:41:11 -0230

    added nginx & preparing deployment

--- server/cron/index.js
+++ server/cron/index.js
@@ -1,6 +1,6 @@
 import { CronJob } from 'cron'
 import { checkChangeEmailRequests } from '../utils/checkChangeEmailRequests'
 
-export const cronJob = new CronJob('* * * * *', async () => {
+export const cronJob = new CronJob('*/30 * * * *', async () => {
   await checkChangeEmailRequests()
 })
--- server/utils/checkChangeEmailRequests.js
+++ server/utils/checkChangeEmailRequests.js
@@ -1,7 +1,7 @@
 import ChangeEmailModel from '../models/changeEmail'
 
 export const checkChangeEmailRequests = async () => {
-  const prevDate = new Date(new Date().setDate(new Date().getDate() - 3))
+  const prevDate = new Date(new Date().setDate(new Date().getDate() - 30))
   const requests = await ChangeEmailModel.find({createdAt: { $lte: prevDate }})
   for (const request of requests) {
     await request.completeRequest()
